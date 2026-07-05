c5a4cbc45fd41eabca123db1a69ebc4b0095bec1
ccathlete <ccathlete01@gmail.com>
2026-07-05 17:25:01 +0200

    write(file): update
    Changes to be committed:
          new file:   commit-tool/src/main/scala/commitool/infrastructure/adapter/ShellGitCommitRunner.scala

---
+++ commit-tool/src/main/scala/commitool/infrastructure/adapter/ShellGitCommitRunner.scala
@@ -0,0 +1,44 @@
+package commitool.infrastructure.adapter
+
+import commitool.application.ports.GitCommitRunner
+import commitool.domain.models.{CommitValidationError, GitError}
+import java.io.{File, PrintWriter}
+import scala.sys.process._
+import scala.util.control.NonFatal
+
+class ShellGitCommitRunner extends GitCommitRunner {
+
+  override def commit(message: String): Either[CommitValidationError, Unit] = {
+    var tempFile: Option[File] = None
+    try {
+      tempFile = Some(File.createTempFile("commit_message_", ".tmp"))
+      val writer = new PrintWriter(tempFile.get)
+      try {
+        writer.write(message)
+      } finally {
+        writer.close()
+      }
+
+      val stdout = new StringBuilder
+      val stderr = new StringBuilder
+
+      val processLogger = ProcessLogger(
+        (o: String) => stdout.append(o).append("\n"),
+        (e: String) => stderr.append(e).append("\n")
+      )
+
+      val command = s"git commit -F ${tempFile.get.getAbsolutePath}"
+      val exitCode = command ! processLogger
+
+      if (exitCode == 0) {
+        Right(())
+      } else {
+        Left(GitError(stderr.toString.trim))
+      }
+    } catch {
+      case NonFatal(e) => Left(GitError(s"Failed to create temp file or execute git command: ${e.getMessage}"))
+    } finally {
+      tempFile.foreach(_.delete())
+    }
+  }
+}
