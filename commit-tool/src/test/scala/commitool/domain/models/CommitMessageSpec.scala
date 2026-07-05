d0fa3684cfcd9193a1493319186ef35b8ad60c06
ccathlete <ccathlete01@gmail.com>
2026-07-05 17:53:32 +0200

    write(file): update
    Changes to be committed:
          new file:   commit-tool/src/test/scala/commitool/domain/models/CommitMessageSpec.scala

---
+++ commit-tool/src/test/scala/commitool/domain/models/CommitMessageSpec.scala
@@ -0,0 +1,62 @@
+package commitool.domain.models
+
+import commitool.domain.models.{CommitMessage, CommitType, InvalidFormat, MissingScope, UnknownType, EmptyBody}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class CommitMessageSpec extends AnyFlatSpec with Matchers {
+
+  "CommitMessage.fromRaw" should "parse valid message correctly" in {
+    val rawMessage = """
+      feat(scope): description
+      
+      body content
+      more body content
+      """
+
+    CommitMessage.fromRaw(rawMessage) should matchPattern {
+      case Right(CommitMessage(CommitType.Feat, "scope", "description", body)) if body == "body content\nmore body content" =>
+    }
+  }
+
+  it should "return InvalidFormat for missing scope" in {
+    val rawMessage = "feat: description"
+    CommitMessage.fromRaw(rawMessage) should matchPattern {
+      case Left(MissingScope(line)) if line == "feat: description" =>
+    }
+  }
+
+  it should "return UnknownType for invalid type" in {
+    val rawMessage = "invalid(scope): description"
+    CommitMessage.fromRaw(rawMessage) should matchPattern {
+      case Left(UnknownType(got, _)) if got == "invalid" =>
+    }
+  }
+
+  it should "return EmptyBody for missing body" in {
+    val rawMessage = "feat(scope): description"
+    CommitMessage.fromRaw(rawMessage) should matchPattern {
+      case Left(EmptyBody) =>
+    }
+  }
+
+  it should "return InvalidFormat for empty input" in {
+    CommitMessage.fromRaw("") should matchPattern {
+      case Left(InvalidFormat("")) =>
+    }
+  }
+
+  it should "handle edge cases correctly" in {
+    // Test with special characters in description
+    val specialCharsMessage = "feat(scope): description with !@#$%^&*()"
+    CommitMessage.fromRaw(specialCharsMessage + "\n\nbody") should matchPattern {
+      case Right(CommitMessage(_, _, description, _)) if description == "description with !@#$%^&*()" =>
+    }
+
+    // Test with multi-line body
+    val multiLineBodyMessage = "feat(scope): description\n\nline1\nline2\nline3"
+    CommitMessage.fromRaw(multiLineBodyMessage) should matchPattern {
+      case Right(CommitMessage(_, _, _, body)) if body == "line1\nline2\nline3" =>
+    }
+  }
+}
